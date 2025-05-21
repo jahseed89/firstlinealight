f7e193efb23bcbb2b86d2967b512b3f4f7ad070d
Samson Ocran <samsonoc@futureconcerns.net>
2025-05-21 13:57:19 +0100

    implementing about us page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div>
-      About Us Page
+      <div>
+        <h1>Contact with Us</h1>
+        <p>Plan Your Journey with Rent a Car</p>
+      </div>
+      <div>
+        <div className="flex">
+          <div>
+            <span>First Name*</span>
+            <input type="text" />
+          </div>
+          <div>
+            <span>Last Name*</span>
+            <input type="text" />
+          </div>
+        </div>
+        <div></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
